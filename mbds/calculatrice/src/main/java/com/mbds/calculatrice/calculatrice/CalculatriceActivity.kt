cdcf272c18ce0f96eb5f908232f2f9b98cf61f79
JALAL Reda <reda.jalal@soprasteria.com>
2020-10-24 12:52:57 +0200

    projet base

---
+++ mbds/calculatrice/src/main/java/com/mbds/calculatrice/calculatrice/CalculatriceActivity.kt
@@ -0,0 +1,15 @@
+package com.mbds.calculatrice.calculatrice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.mbds.calculatrice.databinding.ActivityMainBinding
+
+class CalculatriceActivity {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var operation: String
+
+}
